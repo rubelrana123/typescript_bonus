3311e3fb1cd04a2e47c2108c7f03b0ebed945bf3
rubelrana123 <rubelranatpi707458@gmail.com>
2023-05-13 02:27:03 +0600

    added export

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { add as addnunber, substract} from "./sunstract";
+const add =(param1 : number , param2  : number) : number =>{
+   return param1 + param2    
+}
+const result = add(5,6);
+console.log(result);
+console.log(substract(4,6));
+console.log(addnunber(5,5)
+)
---
+++ src/sunstract.ts
@@ -0,0 +1,7 @@
+export const add = (param1 : number, param2 : number) => {
+    return param1  + param2;
+}
+export const substract = (param1 : number, param2 : number) => {
+    return param1  - param2;
+}
+// console.log(add(5,6))
